90bc8cbf5c2a1a99badb91fc7dd5c56ad660c8ce
Jeremy <vaugoyeau.jeremy@gmail.com>
2022-06-14 17:29:48 +0200

    mise à jour de la page accueil

--- projet_symfony/src/Controller/PageController.php
+++ projet_symfony/src/Controller/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CategoriesRepository;
+use App\Repository\FournisseurRepository;
 use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,14 +12,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class PageController extends AbstractController
 {
     #[Route('/', name: 'app_accueil')]
-    public function index(ProduitRepository $produitRepository,CategoriesRepository $categoriesRepository, string $ProduitDir): Response
+    public function index(ProduitRepository $produitRepository,CategoriesRepository $categoriesRepository, string $ProduitDir, FournisseurRepository $fournisseurRepository, string $DirFour): Response
     {   
+        $fournisseurs = $fournisseurRepository->findAll();
         $produits = $produitRepository->findBy(['groupProduit'=>'PDM']);
         $getcaract = $categoriesRepository->getListCategories();
         return $this->render('front/page/index.html.twig', [
             'getcaract'=> $getcaract,
             'produits'=> $produits,
-            'dir' => $ProduitDir
+            'dir' => $ProduitDir,
+            'fournisseurs' => $fournisseurs,
+            'fourdir'=> $DirFour
         ]);
     }
 
