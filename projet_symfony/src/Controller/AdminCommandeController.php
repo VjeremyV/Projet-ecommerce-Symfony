2a70c78f8e8dd2c54d940cc7792d949c6e970b56
Manon Barre <manonbarre@orange.fr>
2022-06-15 15:52:53 +0200

    Contenu Commande V1

--- projet_symfony/src/Controller/AdminCommandeController.php
+++ projet_symfony/src/Controller/AdminCommandeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Contenu;
 use App\Repository\ClientsRepository;
 use App\Repository\ContenuRepository;
+use App\Repository\ProduitRepository;
 use App\Repository\CommandesRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -14,63 +15,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class AdminCommandeController extends AbstractController
 {
     #[Route('/admin/commande', name: 'app_admin_commande')]
-    public function index(CommandesRepository $commandesRepository, ClientsRepository $clientsRepository, ContenuRepository $contenuRepository, Request $request): Response
+    public function index(CommandesRepository $commandesRepository, Request $request): Response
     {
-        $commande = $commandesRepository->findAll();
-        $client = $clientsRepository->findAll();
-        // $contenu = $contenuRepository->findAll();
+        $commandes = $commandesRepository->findAll();
+
+        $tabCommande = [];
+        foreach($commandes as $commande) {
+            $tabCommande[] = ["id"=> $commande->getId(),"date"=>$commande->getCreatedAt(),"client"=>$commande->getClient()];
+        }
 
         return $this->render('admin_commande/index.html.twig', [
-            'commandes' => $commande,
-            'clients' => $client,
-            // 'contenus' => $contenu
-        ]);
-    }
-
-    #[Route('/admin/produit/update{nom_search}', name: 'admin_produit_updateListe')]
-    public function indexup(CommandesRepository $produitRepository, Request $request, $nom_search = ''): Response
-    {
-        $search = $request->query->get('search');
-        $options = [];
-        if ($id_search = $request->query->get('id_search')) {
-            $options['id_search'] = $id_search;
-        }
-        if ($nom_search = $request->query->get('nom_search')) {
-            $options['nom_search'] = $nom_search;
-        }
-        if ($montant_search = $request->query->get('montant_search')) {
-            $options['montant_search'] = $montant_search;
-        }
-
-        //pagination
-        $offset = max(0, $request->query->getInt('offset', 0));
-        $paginator = $produitRepository->getPaginator($offset, $search, $options);
-        $nbrePages = ceil(count($paginator) / CommandesRepository::PAGINATOR_PER_PAGE);
-        $next = min(count($paginator), $offset + CommandesRepository::PAGINATOR_PER_PAGE);
-        $pageActuelle = ceil($next / CommandesRepository::PAGINATOR_PER_PAGE);
-        $difPages = $nbrePages - $pageActuelle;
-        return $this->render('admin_commande/index.html.twig', [
-            'listcommande' => $paginator,
-            'search' => $search,
-            'previous' => $offset - CommandesRepository::PAGINATOR_PER_PAGE,
-            'offset' => CommandesRepository::PAGINATOR_PER_PAGE,
-            'next' => $next,
-            'nbrePages' => $nbrePages,
-            'pageActuelle' => $pageActuelle,
-            'difPages' => $difPages,
+            'commandes' => $tabCommande,
         ]);
     }
 
     #[Route('/admin/commande/{id}', name: 'app_admin_commande_view')]
-    public function view(CommandesRepository $commandesRepository, ClientsRepository $clientsRepository, ContenuRepository $contenuRepository)
+    public function view(CommandesRepository $commandesRepository, ContenuRepository $contenuRepository)
     {
-        // $contenu = $contenuRepository->findAll();
         $commande = $commandesRepository->findAll();
-        $client = $clientsRepository->findAll();
+        // $contenu = $commande->getContenu();
 
         return $this->render('admin_commande/view.html.twig', [
             // 'contenus' => $contenu,
-            'clients' => $client,
             'commandes' => $commande,
         ]);
     }
