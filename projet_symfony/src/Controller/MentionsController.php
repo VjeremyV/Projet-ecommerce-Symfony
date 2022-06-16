73e5afc56041d81613e34362e6cf5ef8677b6a52
Benjamin <bjd.salaun@gmail.com>
2022-06-16 16:24:24 +0200

    Liens Légaux

--- projet_symfony/src/Controller/MentionsController.php
+++ projet_symfony/src/Controller/MentionsController.php
@@ -7,14 +7,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class CgvController extends AbstractController
+class MentionsController extends AbstractController
 {
-    #[Route('/cgv', name: 'app_cgv')]
+    #[Route('/mentions', name: 'app_mentions')]
     public function index(CategoriesRepository $categoriesRepository): Response
     {
         $getCategories = PageController::Menu($categoriesRepository);
 
-        return $this->render('front/page/cgv/index.html.twig', [
+        return $this->render('front/page/mentions/index.html.twig', [
             'categories' => $getCategories,
         ]);
     }
