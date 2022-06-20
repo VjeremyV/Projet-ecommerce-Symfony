199b288e4118048d4939d008aef6410d55ca163a
Magydream <87599508+Magydream@users.noreply.github.com>
2022-06-20 16:42:53 +0200

    btn commande

--- projet_symfony/migrations/Version20220620131807.php
+++ projet_symfony/migrations/Version20220620131807.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220620124104 extends AbstractMigration
+final class Version20220620131807 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,18 +20,19 @@ final class Version20220620124104 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE group_produit (id INT AUTO_INCREMENT NOT NULL, nom VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE produit_group_produit (produit_id INT NOT NULL, group_produit_id INT NOT NULL, INDEX IDX_81FB1EF5F347EFB (produit_id), INDEX IDX_81FB1EF5D8F91F49 (group_produit_id), PRIMARY KEY(produit_id, group_produit_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE produit_group_produit ADD CONSTRAINT FK_81FB1EF5F347EFB FOREIGN KEY (produit_id) REFERENCES produit (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE produit_group_produit ADD CONSTRAINT FK_81FB1EF5D8F91F49 FOREIGN KEY (group_produit_id) REFERENCES group_produit (id) ON DELETE CASCADE');
-        $this->addSql('DROP TABLE group_produit_produit');
+        $this->addSql('ALTER TABLE produit DROP group_produit');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE group_produit_produit (group_produit_id INT NOT NULL, produit_id INT NOT NULL, INDEX IDX_51CDFA87D8F91F49 (group_produit_id), INDEX IDX_51CDFA87F347EFB (produit_id), PRIMARY KEY(group_produit_id, produit_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
-        $this->addSql('ALTER TABLE group_produit_produit ADD CONSTRAINT FK_51CDFA87D8F91F49 FOREIGN KEY (group_produit_id) REFERENCES group_produit (id) ON DELETE CASCADE');
-        $this->addSql('ALTER TABLE group_produit_produit ADD CONSTRAINT FK_51CDFA87F347EFB FOREIGN KEY (produit_id) REFERENCES produit (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE produit_group_produit DROP FOREIGN KEY FK_81FB1EF5D8F91F49');
+        $this->addSql('DROP TABLE group_produit');
         $this->addSql('DROP TABLE produit_group_produit');
+        $this->addSql('ALTER TABLE produit ADD group_produit VARCHAR(255) DEFAULT NULL');
     }
 }
